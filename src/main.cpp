2ae137164300548940da05725de4f468a65cbd86
naitikshaxma <naitik.sharma_cs.aiml24@gla.ac.in>
2026-02-28 22:15:27 +0530

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,22 @@
 /**
  * @file main.ino
  * @brief Embedded Force Measurement System using FSR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Naitik Sharma
+ * @date 2026-02-28
  *
- * @details
- * Reads analog force data from FSR sensor and
- * displays structured output via Serial Monitor.
+ * Reads analog force data from FSR sensor.
  */
 
- // TODO 1:
- // Define FSR analog pin (Use A0)
+// TODO 1:
+// Define FSR analog pin (Use A0)
+#define FSR_PIN A0
 
- // TODO 2:
- // Create variable to store sensor reading
+// TODO 2:
+// Create variable to store sensor reading
+int fsrValue = 0;
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Print system initialization message
 }
 
 void loop() {
-
-    // TODO 5:
-    // Read analog value from FSR
-
-    // TODO 6:
-    // Print raw ADC value
-
-    // TODO 7:
-    // Apply simple threshold logic (e.g., detect pressure)
-
-    // TODO 8:
-    // Print pressure detection message
-
-    // TODO 9:
-    // Add delay (500ms or 1 second)
 }
