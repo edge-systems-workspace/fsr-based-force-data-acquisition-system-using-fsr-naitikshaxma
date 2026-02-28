acb482d5dba52440667574f0a159e10799128f8e
naitikshaxma <naitik.sharma_cs.aiml24@gla.ac.in>
2026-02-28 22:15:46 +0530

    commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
 #include <Arduino.h>
+
 /**
  * @file main.ino
  * @brief Embedded Force Measurement System using FSR
  * @author Naitik Sharma
  * @date 2026-02-28
- *
- * Reads analog force data from FSR sensor.
  */
 
-// TODO 1:
-// Define FSR analog pin (Use A0)
 #define FSR_PIN A0
-
-// TODO 2:
-// Create variable to store sensor reading
 int fsrValue = 0;
 
 void setup() {
+
+    // TODO 3:
+    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 }
 
 void loop() {
